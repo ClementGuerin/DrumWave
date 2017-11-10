dd9450f6b5282b2a01203aab17ea2edee8b7e8d9
orphevs <ticlem27270@gmail.com>
2017-11-10 18:54:37 +0100

    Script.js fixed + Code JS changé

--- js/script.js
+++ js/script.js
@@ -6,8 +6,24 @@ function checkKey(){
 
     if (Khey == '38') {
         document.getElementById("grid").classList.add('BOOST');
-        console.log('Ish ish');
+    }
+    else{
+        document.getElementById("grid").classList.remove('BOOST');
     }
 }
 
 document.onkeydown = checkKey;
+
+/** 
+        
+        Ton code bro, même si déso mais celui du mec de StackOverflow est better
+
+        document.addEventListener('keydown', function(event){
+        var vroom = document.querySelector('.grid');
+        vroom.classList.add("BOOST");
+        });
+        document.addEventListener('keyup', function(event){
+        var vroom = document.querySelector('.grid');
+        vroom.classList.remove("BOOST");
+        }); 
+**/
