5477932d307614ad833b2f8128df0d9daf3ff11b
orphevs <ticlem27270@gmail.com>
2017-11-10 20:37:54 +0100

    Numpad fonctionnel CSS + JS
    Le Numpad affiche les touches pressées

--- js/script.js
+++ js/script.js
@@ -1,22 +1,103 @@
-
+document.body.addEventListener("keydown", checkKey);
+document.body.addEventListener("keyup", resetKey);
 
 function checkKey(){
     
     var Khey = event.keyCode;
 
-    if (Khey == '38') {
-        document.getElementById("grid").classList.add('BOOST');
-    }
-    else{
-        document.getElementById("grid").classList.remove('BOOST');
-    }
+    
+   // console.log(event.keyCode); // Affiche le keyCode de la touche pressé dans la console
+    
+        if (Khey == '97' || Khey == '98' || Khey == '99' || Khey == '100' || Khey == '101' || Khey == '102' || Khey == '103' || Khey == '104' || Khey == '105') {
+            document.getElementById("grid").classList.add('BOOST');
+        }
+    
+        if (Khey == '97'){
+            document.getElementById("number-numpad-1").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '98'){
+            document.getElementById("number-numpad-2").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '99'){
+            document.getElementById("number-numpad-3").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '100'){
+            document.getElementById("number-numpad-4").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '101'){
+            document.getElementById("number-numpad-5").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '102'){
+            document.getElementById("number-numpad-6").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '103'){
+            document.getElementById("number-numpad-7").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '104'){
+            document.getElementById("number-numpad-8").classList.add('number-numpad-active');
+        }
+    
+        if (Khey == '105'){
+            document.getElementById("number-numpad-9").classList.add('number-numpad-active');
+        }
 }
 
-document.onkeydown = checkKey;
+function resetKey(){
+    
+        var Khey = event.keyCode;
+    
+        if (Khey == '97' || Khey == '98' || Khey == '99' || Khey == '100' || Khey == '101' || Khey == '102' || Khey == '103' || Khey == '104' || Khey == '105') {
+            document.getElementById("grid").classList.remove('BOOST');
+        }
+    
+        if (Khey == '97'){
+            document.getElementById("number-numpad-1").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '98'){
+            document.getElementById("number-numpad-2").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '99'){
+            document.getElementById("number-numpad-3").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '100'){
+            document.getElementById("number-numpad-4").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '101'){
+            document.getElementById("number-numpad-5").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '102'){
+            document.getElementById("number-numpad-6").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '103'){
+            document.getElementById("number-numpad-7").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '104'){
+            document.getElementById("number-numpad-8").classList.remove('number-numpad-active');
+        }
+    
+        if (Khey == '105'){
+            document.getElementById("number-numpad-9").classList.remove('number-numpad-active');
+        }
+}
+
 
 /** 
         
-        Ton code bro, même si déso mais celui du mec de StackOverflow est better
+        Ton code bro :
 
         document.addEventListener('keydown', function(event){
         var vroom = document.querySelector('.grid');
